9db02befa6c40a61a3521a8649db6469a986f45a
kiennt <kien.nguyen@encapital.io>
2023-09-29 10:07:54 +0700

    first commit

---
+++ lib/web_assembly.dart
@@ -0,0 +1,25 @@
+library web_assembly;
+
+import 'package:web_assembly/wasm/wasm_loader.dart';
+
+class WebCounter {
+  late WasmLoader _loader;
+  var counter = 0;
+
+  WebCounter() {
+    _loader = WasmLoader(path: 'assets/wasm/release.wasm');
+    _loaded();
+  }
+
+  Future<bool> _loaded() async {
+    return await _loader.initialized();
+  }
+
+  void incrementCounter() {
+    counter = _loader.callfunc('plusOne', counter) as int;
+  }
+
+  void decrementCounter() {
+    counter = _loader.callfunc('minusOne', counter) as int;
+  }
+}
